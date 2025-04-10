a2ed545fe928fb9234ced88ec78c1b72757a774d
samanthagburek <sgg2056@nyu.edu>
2025-04-10 13:20:02 -0400

    Initial commit

---
+++ src/test/java/com/example/stockthingjava/StockThingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockthingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockThingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
